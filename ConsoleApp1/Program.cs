9cd62d383086fff0dbd14a801b519932323f3fda
Timen Bobnar <61047163+timenbob@users.noreply.github.com>
2024-03-06 19:58:43 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            Console.WriteLine("Tesdfght");
             Console.ReadLine();
             Console.WriteLine("Test2");
         }
